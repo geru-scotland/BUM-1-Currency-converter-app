7e9fc6743843104d2777eee505e30203f6676197
Eideann <aingeru.scotland@gmail.com>
2023-02-05 20:46:07 +0100

    First approach to the cli refactoring; separate business logic.

--- src/main/java/eus/ehu/bum1_fx/CalculatorStarter.java
+++ src/main/java/eus/ehu/bum1_fx/CalculatorStarter.java
@@ -1,8 +1,6 @@
 package eus.ehu.bum1_fx;
 
-import eus.ehu.bum1_fx.business_logic.CommissionCalculator;
-import eus.ehu.bum1_fx.business_logic.Currency;
-import eus.ehu.bum1_fx.business_logic.ForexOperator;
+import eus.ehu.bum1_fx.business_logic.*;
 
 import java.util.InputMismatchException;
 import java.util.Locale;
@@ -10,10 +8,12 @@ import java.util.Scanner;
 
 public class CalculatorStarter {
 
+	private static ExchangeCalculator bizLogic;
+
 	public static void printValidCurrencies() {
 		System.out.println("Valid currencies with their codes are listed below.");
 		int i = 1;
-		for (String name : Currency.longNames()) {
+		for (String name : bizLogic.getCurrencyLongNames()) {
 			if (i%4 == 0)
 				System.out.println();
 			System.out.printf("%-30s", name);
@@ -23,7 +23,7 @@ public class CalculatorStarter {
 	}
 
 	public static void main(String[] args) {
-
+		bizLogic = new BarcenaysCalculator();
 		Scanner input = new Scanner(System.in);
 		input.useLocale(Locale.ENGLISH);
 
@@ -36,22 +36,17 @@ public class CalculatorStarter {
 		double origAmount = 0.0;
 		String endCurrency = "";
 
-		boolean waiting = true;
-		while (waiting) {
-			try {
-				System.out.println("\nPlease indicate the currency that you intend to exchange "
-						+ "(international 3 letter code):");
-				origCurrency = input.next();
-				Currency.valueOf(origCurrency);
-				waiting = false;
-
-			} catch (IllegalArgumentException e) {
-				System.out.printf("\"%s\" could not be recognized as a known code.\n\n", origCurrency);
-				printValidCurrencies();
-			}
+		printValidCurrencies();
+		System.out.println("\nPlease indicate the currency that you intend to exchange "
+				+ "(international 3 letter code):");
+		try{
+			origCurrency = input.next();
+			bizLogic.ValueOf(origCurrency);
+		} catch(IllegalArgumentException e){
+			System.out.println(e.getMessage());
 		}
 
-		waiting = true;
+		boolean waiting = true;
 		while (waiting) {
 			try {
 				System.out.printf("How many %s do you plan to exchange?:%n", origCurrency);
@@ -63,27 +58,17 @@ public class CalculatorStarter {
 			}
 		}
 
-		waiting = true;
-		while (waiting) {
-			try {
-				System.out.printf("Please indicate the currency to which you want to exchange "
-						+ "your %s %.2f (international 3 letter code):\n", origCurrency, origAmount);
-				endCurrency = input.next();
-				Currency.valueOf(endCurrency);
-				waiting = false;
-
-			} catch (IllegalArgumentException e) {
-				System.out.printf("\"%s\" could not be recognized as a known code.\n\n", origCurrency);
-				printValidCurrencies();
-			}
+		System.out.printf("Please indicate the currency to which you want to exchange "
+				+ "your %s %.2f (international 3 letter code):\n", origCurrency, origAmount);
+		try{
+			endCurrency = input.next();
+			bizLogic.ValueOf(endCurrency);
+		} catch(IllegalArgumentException e){
+			System.out.println(e.getMessage());
 		}
-
-		ForexOperator operator = new ForexOperator(origCurrency, origAmount, endCurrency);
 		try {
-			double endAmount = operator.getChangeValue();
-			CommissionCalculator calculator = new CommissionCalculator(endAmount,
-					endCurrency);
-			endAmount -= calculator.calculateCommission();
+			double endAmount = bizLogic.getChangeValue(origCurrency, origAmount, endCurrency);
+			endAmount -= bizLogic.calculateCommission(endAmount, endCurrency);
 			System.out.printf("\nYou can obtain a net exchange value of %s %.2f.%n", endCurrency, endAmount);
 			System.out.println("You can make it effective at any BARCENAYS CAPITAL office.");
 		} catch (Exception e1) {
--- src/main/java/eus/ehu/bum1_fx/business_logic/ExchangeCalculator.java
+++ src/main/java/eus/ehu/bum1_fx/business_logic/ExchangeCalculator.java
@@ -4,4 +4,5 @@ public interface ExchangeCalculator {
     String[] getCurrencyLongNames();
     double getChangeValue(String cur1, double amount, String cur2) throws Exception;
     double calculateCommission(double amount, String origCurrency) throws Exception;
+    void ValueOf(String name) throws IllegalArgumentException;
 }
